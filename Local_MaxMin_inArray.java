802c6d9843567b6931e4d88a2ac7d640d864b486
Терновская Елизавета Викторовна <ternovskaya_el_v@cs.vsu.ru>
2023-11-02 12:40:13 +0300

    First commit

---
+++ Local_MaxMin_inArray.java
@@ -0,0 +1,9 @@
+public class Local_MaxMin_inArray {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to program Finding count of local Max and Min in array!");
+
+
+
+    }
+}
